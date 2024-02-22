7f5e99d4f8ad5ae035314d9b8c54f26b01dc672f
Ihor Havryliv <ihor.havryliv@student.tuke.sk>
2024-02-22 09:21:11 +0100

    add_a+b_funktion

--- lesson_21_02_2024/checking.py
+++ lesson_21_02_2024/checking.py
@@ -1,11 +1,15 @@
+def checking(min, max):
+    while True:
+        try:
+            number = int(input("Enter number in diapason {} -> {}: ".format(min, max)))
+            if -min <= number <= max:
+                print("correct number")
+                break
+
+        except ValueError:
+            print("incorrect value")
 
 
-while True:
-    try:
-        number = int(input("Enter number in diapason -10 -> 10: "))
-        if -10 <= number <= 10:
-            print("correct number")
-            break
-
-    except ValueError:
-        print("incorrect value")
+up_diapasnon = 10
+bottom_diapason = -10
+checking(bottom_diapason,up_diapasnon)
